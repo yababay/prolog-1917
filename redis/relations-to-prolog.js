4317083c186f575d5dc52a66a6b1de5d8c16665a
mabel <bigbelk@live.ru>
2021-02-10 00:28:27 +0300

    relations.md

--- redis/relations-to-prolog.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-fs.readFileSync('relations.txt').toString().split(/\r?\n/g).forEach(line => {
-    if(/^\#/.test(line) || !line.trim()) return
-    const [person1, person2, link] = line.trim().split(',').map(el => el.trim().replace(/^\%\ +/, ''))
-    if(!person2) return
-    console.log(`связь("${person1}", "${person2}", ${!link ? "hz" : '"' + link + '"'}).`)
-})
-
