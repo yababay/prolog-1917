5c8ba01ad3685c0b3bede8b4b208bc8d5ffd919d
mabel <bigbelk@live.ru>
2021-02-09 23:43:37 +0300

    wiki

---
+++ prolog/persons-1917/index.pl
@@ -0,0 +1,3 @@
+:- module(silver_age, [родственники/2, персоны/1]).
+:- use_module("./funcs/people.pl").
+
---
+++ redis/persons-to-prolog.js
@@ -0,0 +1,9 @@
+const getPersons = require('./persons')
+
+;(async function(){
+    const persons = await getPersons()
+    for(const person of persons){
+        const {name, gender, birthYear, deathYear} = person
+        console.log(`персона("${name}", "${gender}", ${birthYear || 0}, ${deathYear || 0}).`)
+    }
+})()
